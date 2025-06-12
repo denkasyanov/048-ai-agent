223ba6009be800a5104ad19214302ed89a00714b
Den Kasyanov <den@den.ai>
2025-06-12 19:06:48 +0400

    Format with ruff and gitignore stuff

--- main.py
+++ main.py
@@ -21,13 +21,10 @@ is_verbose = len(sys.argv) > 2 and sys.argv[2] == "--verbose"
 if is_verbose:
     print(f"User prompt: {user_prompt}")
 
-messages = [
-    types.Content(role="user", parts=[types.Part(text=user_prompt)])
-]
+messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
 
 response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents=messages
+    model="gemini-2.0-flash-001", contents=messages
 )
 
 print(response.text)
